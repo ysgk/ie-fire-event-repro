0431fde8e6941feff58f1b35751210d412e78fd1
ysgk <ysgk@users.noreply.github.com>
2019-12-31 16:49:58 +0900

    Add Element.prototype.match polyfill

--- karma-entry.js
+++ karma-entry.js
@@ -3,3 +3,8 @@ import 'core-js/stable'
 const testsContext = require.context('./src/', true, /.*spec\.js$/)
 
 testsContext.keys().forEach(testsContext)
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#polyfill
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector
+}
